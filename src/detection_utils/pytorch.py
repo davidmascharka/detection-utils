1a87ef4acfca9afeb34f17c8774bb099843edd99
David Mascharka <davidmascharka@gmail.com>
2019-03-07 10:41:50 -0500

    Add reduction parameter to focal loss (#11)

--- src/detection_utils/pytorch.py
+++ src/detection_utils/pytorch.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn.functional as F
 
 
-def softmax_focal_loss(input, target, alpha=1, gamma=0):
+def softmax_focal_loss(input, target, alpha=1, gamma=0, reduction='mean'):
     """ Returns the focal loss as described in https://arxiv.org/abs/1708.02002 which is given by -ɑ(1-p)ˠlog(p).
 
     Parameters
@@ -19,17 +19,22 @@ def softmax_focal_loss(input, target, alpha=1, gamma=0):
     gamma : Real, optional (default=0)
         The Ɣ focusing parameter.
 
+    reduction : str ∈ {'mean', 'sum', 'none'}, optional (default='mean')
+        How to reduce the loss to a scalar, or 'none' to return the per-item loss.
+
     Returns
     -------
-    torch.Tensor
+    torch.Tensor, shape=() if reduction is 'none' otherwise shape=(N,)
         The mean focal loss.
 
     Notes
     -----
     When ɑ=1 and Ɣ=0, this is equivalent to softmax cross-entropy.
     """
+    assert reduction in {'mean', 'sum', 'none'}, 'Valid reduction strategies are "mean," "sum," and "none"'
+
     input = F.log_softmax(input, dim=1)
     logpc = input[(range(len(target)), target)]
     one_m_pc = (-1 * torch.expm1(logpc)).clamp(min=1e-14, max=1.0)
     loss = -alpha * one_m_pc**gamma * logpc
-    return loss.mean()
+    return loss if reduction == 'none' else loss.mean() if reduction == 'mean' else loss.sum()
--- tests/test_pytorch.py
+++ tests/test_pytorch.py
@@ -52,7 +52,7 @@ class Test_focal_loss:
                         atol=1e-6, rtol=1e-6, err_msg="Focal loss with gamma=0 fails to match cross-entropy loss.")
 
     @given(inputs=hnp.arrays(dtype=float, shape=hnp.array_shapes(min_dims=2, max_dims=2, min_side=2),
-                             elements=st.floats(0, 1e1)),
+                             elements=st.floats(0, 10)),
            alpha=st.floats(-10, 10),
            gamma=st.floats(0, 10),
            dtype=st.sampled_from((torch.float32, torch.float64)),
@@ -123,3 +123,62 @@ class Test_focal_loss:
 
         loss.backward()
         assert not np.any(np.isnan(inputs.grad.numpy())), "focal loss gradient is nan"
+
+    @pytest.mark.parametrize(
+        ("reduction_name", "reduction_function"),
+        [("mean", torch.mean), ("sum", torch.sum), ("none", None)],
+    )
+    @given(
+        inputs=hnp.arrays(
+            dtype=float,
+            shape=hnp.array_shapes(min_dims=2, max_dims=2, min_side=2),
+            elements=st.floats(0, 10)),
+        alpha=st.floats(-10, 10),
+        gamma=st.floats(0, 10),
+        dtype=st.sampled_from((torch.float32, torch.float64)),
+        data=st.data())
+    @settings(deadline=None)
+    def test_reductions(self, reduction_name, reduction_function,
+                        inputs, alpha, gamma, dtype, data):
+        """ Ensure the reductions do what we expect them to. """
+        targets = data.draw(hnp.arrays(dtype=int,
+                                       shape=(inputs.shape[0],),
+                                       elements=st.integers(0, inputs.shape[1] - 1)),
+                            label="targets")
+
+        inputs1 = tensor(inputs, dtype=dtype, requires_grad=True)
+        inputs2 = tensor(inputs, dtype=dtype, requires_grad=True)
+        targets = tensor(targets, dtype=torch.int64)
+
+        # numerically-stable focal loss
+        loss = softmax_focal_loss(inputs1, targets, alpha=alpha, gamma=gamma, reduction=reduction_name)
+
+        # naive focal loss
+        input = F.softmax(inputs2, dim=1)
+        pc = input[(range(len(targets)), targets)]
+        naive_loss = -alpha * (1 - pc) ** gamma * torch.log(pc)
+        if reduction_function is not None:
+            naive_loss = reduction_function(naive_loss)
+
+        assert_allclose(
+            actual=loss.detach().numpy(),
+            desired=naive_loss.detach().numpy(),
+            atol=1e-5,
+            rtol=1e-5,
+            err_msg="focal loss with reduction='{}' does not match naive "
+            "implementation on numerically-stable domain".format(reduction_name),
+        )
+
+        if naive_loss.dim() == 0:
+            loss.backward()
+            naive_loss.backward()
+            assert_allclose(
+                actual=inputs1.grad.numpy(),
+                desired=inputs2.grad.numpy(),
+                atol=1e-5,
+                rtol=1e-5,
+                err_msg="focal loss gradient with reduction='{}' does not "
+                "match that of naive loss on numerically-stable domain".format(
+                    reduction_name
+                ),
+            )
