5387692efccc18dea65a18a6c5f5c8142b25b105
Arjun Majumdar <arjunmajum@gmail.com>
2019-06-14 15:34:31 -0400

    Fix distribution statement

--- src/detection_utils/pytorch.py
+++ src/detection_utils/pytorch.py
@@ -1,10 +1,9 @@
-# DISTRIBUTION STATEMENT A. Approved for public release: distribution unlimited.
+# DISTRIBUTION STATEMENT A. Approved for public release. Distribution is unlimited.
 #
-# This material is based upon work supported by the Assistant Secretary of Defense for Research and
-# Engineering under Air Force Contract No. FA8721-05-C-0002 and/or FA8702-15-D-0001. Any opinions,
-# findings, conclusions or recommendations expressed in this material are those of the author(s) and
-# do not necessarily reflect the views of the Assistant Secretary of Defense for Research and
-# Engineering.
+# This material is based upon work supported by the Under Secretary of Defense for Research and
+# Engineering under Air Force Contract No. FA8702-15-D-0001. Any opinions, findings, conclusions or
+# recommendations expressed in this material are those of the author(s) and do not necessarily
+# reflect the views of the Under Secretary of Defense for Research and Engineering.
 #
 # © 2019 Massachusetts Institute of Technology.
 #
@@ -14,9 +13,9 @@
 #
 # Delivered to the U.S. Government with Unlimited Rights, as defined in DFARS Part 252.227-7013 or
 # 7014 (Feb 2014). Notwithstanding any copyright notice, U.S. Government rights in this work are
-# defined by DFARS 252.227-7013 or DFARS 252.227-7014 as detailed above. Use of this work other than
-# as specifically authorized by the U.S. Government may violate any copyrights that exist in this
-# work.
+# defined by DFARS 252.227-7013 or DFARS 252.227-7014 as detailed above. Use of this work other
+# than as specifically authorized by the U.S. Government may violate any copyrights that exist in
+# this work.
 
 import torch
 from torch import Tensor
