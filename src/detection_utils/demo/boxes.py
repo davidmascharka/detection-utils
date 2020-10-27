985c8c7c1f2d5053f59741347e9b48e53c19dd3c
Ryan Soklaski <ry26099@mit.edu>
2020-10-27 18:06:11 -0400

    add utilities for demo

---
+++ src/detection_utils/demo/boxes.py
@@ -0,0 +1,101 @@
+from typing import Optional
+import numpy as np
+import torch as tr
+
+from ..boxes import non_max_suppression
+
+DEFAULT_BOX_STEP = 16
+DEFAULT_BOX_SIZE = 32
+
+
+def make_anchor_boxes(
+    *, height: int, width: int, box_size=DEFAULT_BOX_SIZE, box_stride=DEFAULT_BOX_STEP
+) -> np.ndarray:
+    anchor_boxes = []
+    for y in range(0, height, box_stride):
+        for x in range(0, width, box_stride):
+            anchor_boxes.append(
+                np.array([-box_size // 2, -box_size // 2, box_size // 2, box_size // 2])
+                + np.array([x, y, x, y])
+            )
+    return np.vstack(anchor_boxes)
+
+
+def compute_detections(
+    classifications: tr.Tensor,
+    regressions: tr.Tensor,
+    feature_map_width: int,
+    anchor_box_step: int = DEFAULT_BOX_STEP,
+    anchor_box_size: int = DEFAULT_BOX_SIZE,
+    score_threshold: Optional[float] = None,
+    nms_threshold: float = 0.3,
+):
+    """ Compute a set of boxes, class predictions, and foreground scores from
+        detection model outputs.
+
+    Parameters
+    ----------
+    classifications : torch.Tensor, shape=(N, R*C, # classes)
+        A set of class predictions at each spatial location.
+
+    regressions : torch.Tensor, shape=(N, R*C, 4)
+        A set of predicted box offsets, in (x, y, w, h) at each spatial location.
+
+    feature_map_width : int
+        The number of pixels in the feature map, along the x direction.
+
+    anchor_box_step : int
+        The number of pixels (in image space) between each anchor box.
+
+    anchor_box_size : int
+        The side length of the anchor box.
+
+    score_threshold: Optional[float]
+        If specified, detections with foreground scores below this
+        threshold are ignored
+
+    nms_threshold: float, optional (default=0.3)
+        The IoU threshold to use for NMS, above which one of two box will be suppressed.
+
+    Returns
+    -------
+    Tuple[numpy.ndarray shape=(R*C, 4), numpy.ndarray shape=(R*C, 1), numpy.ndarray shape=(R*C,)]
+        The (boxes, class predictions, foreground scores) at each spatial location.
+    """
+    box_predictions = np.empty((len(regressions), 4), dtype=np.float32)
+    scores = tr.softmax(classifications, dim=-1).detach().cpu().numpy()
+    scores = 1 - scores[:, 0]  # foreground score
+
+    class_predictions = (
+        classifications.argmax(dim=-1, keepdim=True).detach().cpu().numpy()
+    )
+    regressions = regressions.detach().cpu().numpy()
+
+    y, x = np.divmod(
+        np.arange(len(classifications)), feature_map_width, dtype=np.float32
+    )
+
+    # transform (R*C, 4) to (4, R*C) for assignment
+    (x_reg, y_reg, w_reg, h_reg,) = regressions.T
+    x = anchor_box_step * x + anchor_box_size * x_reg
+    y = anchor_box_step * y + anchor_box_size * y_reg
+
+    half_w = np.clip(np.exp(w_reg), 0, 10 ** 6) * anchor_box_size / 2
+    half_h = np.clip(np.exp(h_reg), 0, 10 ** 6) * anchor_box_size / 2
+
+    box_predictions[:, 0] = x - half_w  # x1
+    box_predictions[:, 1] = y - half_h  # y1
+    box_predictions[:, 2] = x + half_w  # x2
+    box_predictions[:, 3] = y + half_h  # y2
+
+    if score_threshold is not None:
+        keep = scores > score_threshold
+        box_predictions = box_predictions[keep]
+        class_predictions = class_predictions[keep]
+        scores = scores[keep]
+
+    keep_idxs = non_max_suppression(box_predictions, scores, threshold=nms_threshold)
+    box_predictions = box_predictions[keep_idxs]
+    class_predictions = class_predictions[keep_idxs]
+
+    return box_predictions, class_predictions, scores
---
+++ src/detection_utils/demo/pytorch.py
@@ -0,0 +1,142 @@
+from typing import Tuple
+
+import numpy as np
+import torch as tr
+import torch.nn.functional as F
+from torch import Tensor, nn
+
+from ..pytorch import softmax_focal_loss
+
+
+class Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 10, 3, padding=1)
+        self.conv2 = nn.Conv2d(10, 20, 3, padding=1)
+        self.conv3 = nn.Conv2d(20, 30, 3, padding=1)
+        self.conv4 = nn.Conv2d(30, 40, 3, padding=1)
+
+        # background / rectangle / triangle / circle
+        self.classification = nn.Conv2d(40, 4, 1)
+        self.regression = nn.Conv2d(40, 4, 1)
+
+        for layer in (
+            self.conv1,
+            self.conv2,
+            self.conv3,
+            self.conv4,
+            self.classification,
+            self.regression,
+        ):
+            nn.init.xavier_normal_(layer.weight, np.sqrt(2))
+            nn.init.constant_(layer.bias, 0)
+
+        nn.init.constant_(
+            self.classification.bias[0], -4.6
+        )  # rougly -log((1-π)/π) for π = 0.01
+
+    def forward(self, imgs: Tensor) -> Tuple[Tensor, Tensor]:
+        """"
+        Computes the classification scores and bounding box regression associated
+        with each anchor box of each image.
+
+        Parameters
+        ----------
+        imgs : Tensor, shape-(N, 3, H, W)
+            A batch of N images.
+
+        Returns
+        -------
+        classifications, regressions : Tuple[Tensor, Tensor]
+            shape-(N, K, N_class), shape-(N, K, 4)
+            For each of N images in the batch, returns the classification scores
+            and bbox regressions associated with each of the K anchor boxes associated
+            with that image.
+
+        Notes
+        -----
+        The anchor boxes are flattened in row-major order"""
+        imgs = F.max_pool2d(F.relu(self.conv1(imgs)), 2)
+        imgs = F.max_pool2d(F.relu(self.conv2(imgs)), 2)
+        imgs = F.max_pool2d(F.relu(self.conv3(imgs)), 2)
+        imgs = F.max_pool2d(F.relu(self.conv4(imgs)), 2)
+
+        # (N, num-classes, R, C) -> (N, R, C, num-classes)
+        classifications = self.classification(imgs).permute(0, 2, 3, 1)
+        # (N, R, C, num-classes) -> (N, R*C, num-classes)
+        classifications = classifications.reshape(
+            imgs.shape[0], -1, classifications.shape[-1]
+        )
+        # (N, 4, R, C) -> (N, R, C, 4)
+        regressions = self.regression(imgs).permute(0, 2, 3, 1)
+        # (N, R, C, 4) ->  (N, R*C, 4)
+        regressions = regressions.reshape(imgs.shape[0], -1, 4)
+        return classifications, regressions
+
+
+def loss(
+    class_predictions: Tensor,
+    regression_predictions: Tensor,
+    class_targets: Tensor,
+    regression_targets: Tensor,
+) -> Tuple[Tensor, Tensor]:
+
+    """
+    Computes the classification and regression
+      smooth L1 (Huber) loss for regression (only on foreground anchor boxes)
+      softmax focal loss for classification (excluding ignore anchor boxes)
+
+    Parameters
+    ----------
+    class_predictions : Tensor, shape-(N, K, num-class)
+    regression_predictions : Tensor, shape-(N, K, 4)
+    class_targets : Tensor, shape-(N, K)
+    regression_targets : Tensor, shape-(N, K, 4)
+
+    Returns
+    -------
+    classification_loss, regression_loss: Tuple[Tensor, Tensor], shape-() shape-()
+        The mean classification loss and regression loss, respectively.
+
+    Notes
+    -----
+    `N` is the batch size. `K` is the number of anchor boxes associated with
+    each image.
+    """
+    # shape-(N*K,)
+    class_targets = class_targets.reshape(-1)
+
+    # shape-(N*K, 4)
+    regression_targets = regression_targets.reshape(-1, 4)
+
+    # shape-(N*K, num-class)
+    class_predictions = class_predictions.reshape(-1, class_predictions.shape[-1])
+
+    # shape-(N*K, 4)
+    regression_predictions = regression_predictions.reshape(-1, 4)
+
+    mask = tr.squeeze(class_targets > 0)
+    num_foreground = mask.sum().item()
+    if mask.numel() > 0:
+        regression_loss = F.smooth_l1_loss(
+            regression_predictions[mask], regression_targets[mask]
+        )
+    else:
+        regression_loss = tr.tensor(0).float()
+
+    mask = tr.squeeze(class_targets > -1)
+
+    # the sum of focal loss terms is normalized by the number
+    # of anchors assigned to a ground-truth box
+    classification_loss = (
+        softmax_focal_loss(
+            class_predictions[mask],
+            class_targets[mask],
+            alpha=0.25,
+            gamma=2,
+            reduction="sum",
+        )
+        / num_foreground
+    )
+
+    return classification_loss, regression_loss
